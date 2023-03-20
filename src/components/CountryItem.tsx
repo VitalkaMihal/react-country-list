7645bc20e697ad1e50cdd26ed1d5e92fe491b10c
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2023-03-20 22:57:51 +0300

    [project-2] add css style

--- src/components/CountryItem.tsx
+++ src/components/CountryItem.tsx
@@ -13,22 +13,22 @@ export const CountryItem = ({ country }: CountryItemProps) => {
     <ul className="list-group">
       <li className="list-group-item d-flex align-items-center">
         <img src={flag} height={70} width={100} alt="flag" />
-        <p style={{ width: 250 }} className="justify-content-start m-3">
+        <p style={{ width: 250 }} className="d-flex justify-content-start m-3">
           {name}
         </p>
-        <p style={{ width: 200 }} className="justify-content-start">
+        <p style={{ width: 200 }} className="d-flex justify-content-start">
           {capital}
         </p>
-        <p style={{ width: 200 }} className="justify-content-start">
+        <p style={{ width: 200 }} className="d-flex justify-content-start">
           {region}
         </p>
         <Badge
-          color="flex-grow-1 justify-content-center badge bg-primary px-4 py-2 m-2"
+          color="d-flex flex-grow-1 justify-content-center badge bg-primary px-4 py-2 m-2"
           count={area}
           title="area"
         />
         <Badge
-          color="flex-grow-1 justify-content-center badge bg-secondary px-3 py-2 m-2"
+          color="d-flex flex-grow-1 justify-content-center badge bg-secondary px-3 py-2 m-2"
           count={population}
           title="population"
         />
